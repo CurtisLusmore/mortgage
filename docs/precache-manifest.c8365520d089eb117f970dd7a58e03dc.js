47e0032c4feb930ae36d8de083bf3da6260051bd
Curtis Lusmore <curtis@lusmo.re>
2020-07-13 21:13:30 +1000

    Build to docs directory

---
+++ docs/precache-manifest.c8365520d089eb117f970dd7a58e03dc.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fffc7dcde2c7644bc005b16b02021a65",
+    "url": "/index.html"
+  },
+  {
+    "revision": "48218e1959d6ec2103d5",
+    "url": "/static/css/main.8d6a12d2.chunk.css"
+  },
+  {
+    "revision": "d6c77e2533956bd04ac0",
+    "url": "/static/js/2.a74db416.chunk.js"
+  },
+  {
+    "revision": "5ac48c47bb3912b14c2d8de4f56d5ae8",
+    "url": "/static/js/2.a74db416.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "48218e1959d6ec2103d5",
+    "url": "/static/js/main.f10681df.chunk.js"
+  },
+  {
+    "revision": "e1bd3d9f2310ec796dbd",
+    "url": "/static/js/runtime-main.cd296cc8.js"
+  }
+]);
